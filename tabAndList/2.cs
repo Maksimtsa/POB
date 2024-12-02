4a90f0c058d891068a4825d62ea576cdcecc8c6e
Maksim <144340235+Maksimtsa@users.noreply.github.com>
2024-12-02 10:08:05 +0100

    Update 2.cs

--- tabAndList/2.cs
+++ tabAndList/2.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _02._12
 {
@@ -10,44 +6,115 @@ namespace _02._12
     {
         static void Main(string[] args)
         {
-            //pobranie rozmiaru tablicy od użytkownika
-            Console.WriteLine("podaj rozmiar tablicy: ");
-            int size = int.Parse(Console.ReadLine());
-
-            //wywołanie funkcji
-            int[] array = FillArrayRandom(size);
-
-            Console.WriteLine("czy chesz wyświetlić dane t/n");
-            string response = Console.ReadLine().ToLower();
-            if(response == "t")
+            int[] array = null;
+            bool exit = false;
+            while (!exit)
             {
-                DisplayArray(array);
+                DisplayMenu();
+                Console.WriteLine("Twój wybór: ");
+                int choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Podaj rozmiar tablicy: ");
+                        int size = int.Parse(Console.ReadLine());
+                        array = CreateArray(size);
+                        Console.WriteLine("Tablica została utworzona.");
+                        break;
+                    case 2:
+                        if (array != null)
+                        {
+                            FillArrayRandom(array);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Najpierw utwórz tablicę wybierając opcję 1.");
+                        }
+                        break;
+                    case 3:
+                        if (array != null)
+                        {
+                            FillArrayManually(array);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Najpierw utwórz tablicę wybierając opcję 1.");
+                        }
+                        break;
+                    case 4:
+                        if (array != null)
+                        {
+                            DisplayArray(array);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Tablica nie została utworzona.");
+                        }
+                        break;
+                    case 5:
+                        exit = true;
+                        Console.WriteLine("Wyjście z programu...");
+                        break;
+                    default:
+                        Console.WriteLine("Niepoprawny wybór, spróbuj ponownie.");
+                        break;
+                }
             }
-            else
-            {
-                Console.WriteLine("zawartość nie została wyświetlona");
-            }
-            Console.ReadKey();
-
         }
-        //funkcjaa statyczna do losowego wypełniania tablicy
-        static int[] FillArrayRandom(int[] array)
+
+        static int[] CreateArray(int size)
+        {
+            int[] array = new int[size];
+            Random rand = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = rand.Next(1, 101);
+            }
+            return array;
+        }
+
+        static void FillArrayRandom(int[] array)
         {
             Random rand = new Random();
             for (int i = 0; i < array.Length; i++)
             {
                 array[i] = rand.Next(1, 101);
             }
-            Console.WriteLine("Tablica została wyświetlona losowymi wartościami");
+            Console.WriteLine("Tablica została wypełniona losowymi wartościami.");
         }
-        //funkcja wywołania
+
+
+        static void FillArrayManually(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"Podaj wartość dla elementu {i + 1}: ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Tablica została wypełniona ręcznie podanymi wartościami.");
+        }
+
+
         static void DisplayArray(int[] array)
         {
-            foreach(int element in array)
+            Console.WriteLine("Zawartość tablicy:");
+            foreach (int element in array)
             {
-                Console.WriteLine(element + " ");
+                Console.Write(element + " ");
             }
             Console.WriteLine();
+        }
+
+
+        static void DisplayMenu()
+        {
+            Console.WriteLine("Menu");
+            Console.WriteLine("1. Utwórz tablicę");
+            Console.WriteLine("2. Wylosuj zawartość tablicy");
+            Console.WriteLine("3. Dodaj wartości do tablicy");
+            Console.WriteLine("4. Wyświetl tablicę");
+            Console.WriteLine("5. Wyjdź");
+            Console.WriteLine("Wybierz opcję: ");
         }
     }
 }
